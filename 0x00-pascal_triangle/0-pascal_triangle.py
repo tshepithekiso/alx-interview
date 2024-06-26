08be94ff611ca3fd8501c857df2426799413d7d9
tshepithekiso <tshepangthekiso15@gmail.com>
2024-06-26 17:48:39 +0000

    0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,13 +1,25 @@
 def pascal_triangle(n):
     if n <= 0:
         return []
-    
-    triangle = [[1]]
+
+    triangle = [[1]]  # Initialize the first row of Pascal's triangle
+
     for i in range(1, n):
-        row = [1]
+        row = [1]  # First element of each row is always 1
         for j in range(1, i):
             row.append(triangle[i-1][j-1] + triangle[i-1][j])
-        row.append(1)
+        row.append(1)  # Last element of each row is always 1
         triangle.append(row)
-    
+
     return triangle
+
+if __name__ == "__main__":
+    def print_triangle(triangle):
+        """
+        Print the triangle
+        """
+        for row in triangle:
+            print("[{}]".format(",".join([str(x) for x in row])))
+
+    print_triangle(pascal_triangle(5))
+
