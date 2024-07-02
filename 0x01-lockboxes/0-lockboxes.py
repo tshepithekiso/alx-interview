ad47edf415835e37f43441a7291f505896315e9b
tshepithekiso <tshepangthekiso15@gmail.com>
2024-07-02 01:05:22 -0700

    0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,16 +1,27 @@
 #!/usr/bin/python3
+
 def canUnlockAll(boxes):
     n = len(boxes)
-    visited = [False] * n
-    keys = set()
+    opened = [False] * n
+    opened[0] = True
+    keys = [0]  # Start with the first box open
 
-    def dfs(box):
-        if box == 0:
-            visited[0] = True
-        for key in boxes[box]:
-            if key < n and not visited[key]:
-                visited[key] = True
-                dfs(key)
+    while keys:
+        current_key = keys.pop()
+        for key in boxes[current_key]:
+            if key < n and not opened[key]:
+                opened[key] = True
+                keys.append(key)
 
-    dfs(0)
-    return all(visited)
+    return all(opened)
+
+# Testing the function with the provided test cases
+if __name__ == "__main__":
+    boxes = [[1], [2], [3], [4], []]
+    print(canUnlockAll(boxes))  # Expected output: True
+
+    boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+    print(canUnlockAll(boxes))  # Expected output: True
+
+    boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+    print(canUnlockAll(boxes))  # Expected output: False
