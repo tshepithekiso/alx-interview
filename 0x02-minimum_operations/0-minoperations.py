676322ba8d7b4ee1a735bbbed049137ad4776461
tshepithekiso <tshepangthekiso15@gmail.com>
2024-07-09 10:15:34 -0700

    0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
 
+
 def minOperations(n):
     if n <= 1:
         return 0
 
-    dp = [0] * (n + 1)
+    dp = [float('inf')] * (n + 1)
+    dp[1] = 0
 
     for i in range(2, n + 1):
-        dp[i] = i  # Initialize with the maximum possible operations (i)
+        for j in range(1, i // 2 + 1):
+            if i % j == 0:
+                dp[i] = min(dp[i], dp[j] + (i // j))
 
-        j = 1
-        while i * j <= n:
-            dp[i * j] = min(dp[i * j], dp[i] + 1)
-            j += 1
-
-    return dp[n] if dp[n] != n else 0
-
+    return dp[n] if dp[n] != float('inf') else 0
